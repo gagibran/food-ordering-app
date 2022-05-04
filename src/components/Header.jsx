901b03491618fa58a485e7adb746ed3b945dc601
Gabriel Gibran <ggibran@wexhealthinc.com>
2022-05-04 10:48:29 -0300

    Changes the placeholder text in the alt property

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = function () {
         <header className={styles['app-header']}>
             <img
                 src="https://images.unsplash.com/photo-1577303935007-0d306ee638cf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2040&q=80"
-                alt="test"
+                alt="Table full of food."
             />
         </header>
     );
