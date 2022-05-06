470a76d2545ba7a0112058935de00981cb233c4d
Gabriel Alvares Gibran <gagibran@gmail.com>
2022-05-06 16:50:25 -0300

    Moved the constant MENU_ITEMS inside Menu.jsx

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,47 +4,11 @@ import contentStyles from "../styles/Content.module.css";
 
 const CARDS_Y_OFFSET_BASE = 100;
 
-const MENU_ITEMS = [
-    {
-        id: 1,
-        foodName: 'Risotto alla Milanese',
-        description: 'Risotto with Parmesan & Saffron.',
-        price: 15.99
-    },
-    {
-        id: 2,
-        foodName: 'Pasta alla Norma',
-        description: 'Sicilian pasta dish with eggplant, marinara and basil.',
-        price: 10.99
-    },
-    {
-        id: 3,
-        foodName: 'Pizza Margherita',
-        description: 'Tomato sauce, mozzarella and basil.',
-        price: 11.99
-    },
-    {
-        id: 4,
-        foodName: 'Bistecca alla Fiorentina',
-        description: 'Porterhouse steak done Florentine style.',
-        price: 25.99
-    },
-    {
-        id: 5,
-        foodName: 'Ragù alla Bolognese',
-        description: 'Pasta with Bolognese sauce.',
-        price: 7.99
-    },
-];
-
 const Content = function () {
     return (
         <section className={contentStyles['app-content']}>
             <RestaurantDescription componentYOffset={CARDS_Y_OFFSET_BASE * 2} />
-            <Menu
-                componentYOffset={CARDS_Y_OFFSET_BASE}
-                menuItems={MENU_ITEMS}
-            />
+            <Menu componentYOffset={CARDS_Y_OFFSET_BASE} />
         </section>
     );
 };
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,48 @@
-import { number, array } from "prop-types";
+import { number } from "prop-types";
 import Card from "./Card";
 import MenuItem from "./MenuItem";
 import cardStyles from "../styles/Card.module.css";
 
-const Menu = function ({ componentYOffset, menuItems }) {
+// In a full-stack application, we would fetch this data from a database.
+const MENU_ITEMS = [
+    {
+        id: 1,
+        foodName: 'Risotto alla Milanese',
+        description: 'Risotto with Parmesan & Saffron.',
+        price: 15.99
+    },
+    {
+        id: 2,
+        foodName: 'Pasta alla Norma',
+        description: 'Sicilian pasta dish with eggplant, marinara and basil.',
+        price: 10.99
+    },
+    {
+        id: 3,
+        foodName: 'Pizza Margherita',
+        description: 'Tomato sauce, mozzarella and basil.',
+        price: 11.99
+    },
+    {
+        id: 4,
+        foodName: 'Bistecca alla Fiorentina',
+        description: 'Porterhouse steak done Florentine style.',
+        price: 25.99
+    },
+    {
+        id: 5,
+        foodName: 'Ragù alla Bolognese',
+        description: 'Pasta with Bolognese sauce.',
+        price: 7.99
+    },
+];
+
+const Menu = function ({ componentYOffset }) {
     return (
         <Card
             customClass={`${cardStyles[`card--y-${componentYOffset}px-translated`]} ${cardStyles['card--less-padding']}`}
         >
-            {menuItems.map(item => {
+            {MENU_ITEMS.map(item => {
                 return (
                     <MenuItem
                         key={item.id}
@@ -24,8 +58,7 @@ const Menu = function ({ componentYOffset, menuItems }) {
 };
 
 Menu.propTypes = {
-    componentYOffset: number.isRequired,
-    menuItems: array.isRequired
+    componentYOffset: number.isRequired
 };
 
 export default Menu;
