1e79a0f1ae819c39ca216553712abd0c132a5fd8
Gabriel Alvares Gibran <gagibran@gmail.com>
2022-05-11 18:58:08 -0300

    Made some functions better

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,15 @@
+import { useContext } from "react";
 import CartButton from "./CartButton";
 import Cart from "./Cart";
+import { CartContext } from "../store/cartContext";
 import navStyles from "../styles/Nav.module.css";
 
 const Nav = function () {
+    const cartContext = useContext(CartContext)
+
     return (
         <nav className={navStyles['app-nav']}>
-            <Cart />
+            {cartContext.isCartHidden || <Cart />}
             <h1 className={navStyles['app-nav__logo']}>
                 Gabriel's Bistro
             </h1>
